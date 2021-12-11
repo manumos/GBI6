a4d3b68bbd6270f384cace92427421834700c84e
manumos <manuela.moscoso@est.ikiam.edu.ec>
2021-12-10 23:00:54 -0500

    creación del manual del deber y del primer script

---
+++ netsize.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Este script busca determinar el número de filas (polinizadores) y columnas (plantas) del los archivos .txt contenido sen el directorio CSB/unix/data/Saavedra2013.
+#1. Determinar el número de filas = POLINIZADORES
+#2. Determinar el número de columnas = PLANTAS 
