5190d83a5d95bf8ae843bfae23aeab3d6d51da65
manumos <manuela.moscoso@est.ikiam.edu.ec>
2021-12-11 02:21:52 -0500

    He logrado imprimir el conteo con sus titulos

--- netsize.sh
+++ netsize.sh
@@ -3,18 +3,21 @@
 
 ##1. Determinar el número de filas = POLINIZADORES
 
-#selecciono todas las filas del archivo argumento y las almaceno en un archivo temporal
-#head -n +1 $1 > $1.txt
-
-#imprimo el archivo temporal y seguidamente cuento las filas
-#almaceno la respuesta  en un nuevo archivo creado para almacernar los resultados
-cat $1 |wc -l > netsize.txt
+#Creo un nuevo archivo para almacernar los resultados y etiqueto el primer resultado
+echo "POLINIZADORES" > netsize.txt
+#imprimo el archivo argumento y cuenta las filas
+#almaceno la respuesta  en el archivo creado
+cat $1 |wc -l >> netsize.txt
 
 ###########################################################
 ##2. Determinar el número de columnas = PLANTAS
 
+#creo la etiqueta para los sigueintes datos dentro del archivo
+echo "PLANTAS" >> netsize.txt
+
 #selecciono la primera fila de l archivo argumentos $1 y la guardo en el archivo temporal correspondiente
 head -n 1 $1 > $1.txt
+
 
 #imprimo el archivo temporal que contiene la única fila del archivo argumento
 #y elimino los espacios en blanco " "
@@ -26,7 +29,3 @@ rm $1.txt
 
 #imprimo los resultado almacenados
 cat netsize.txt
-
-
-
-
