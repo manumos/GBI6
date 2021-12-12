509d42cb26bf834a862af9ebf11405d5622a5238
manumos <manuela.moscoso@est.ikiam.edu.ec>
2021-12-11 19:34:10 -0500

    he logrado contabilizar lineas y columnas de cada archivo

--- netsize_all.sh
+++ netsize_all.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-#imprimo el primer titulo y creo un archivo nuevo
-#echo "POLINIZADORES" > netsize_all.txt
 #abro el directorio de interes
-#de todos los archivos .txt cuento las lineas y las guardo en el anterior archivo creado
 cd $1
-wc -l *.txt >> temp*.txt
-cat temp*.txt >> netsize_all.txt
-rm temp*.txt
-#imprimo el segundo titulo en el archivo creado
-#echo "PLANTAS" >> netsize_all.txt
+#POLINIZADORES
+#de todos loS archivos .txt cuenta las lineas y las guardo en archivos temporales
+wc -l *.txt > tempA*.txt
+#imprimo todos los archivos temporales en un solo archivo final
+cat tempA*.txt
+
+#PLANTAS
 #selecciono la primera fila de los archivos .txt y la almaceno en un documento temporal
-head -n 1 *.txt > $1.txt
-#imprimo el documento temporal y  borro los espacios en blanco
-#luego cuento los caracteres y lo guardo en el archivo de resultado
-cat $1.txt | tr -d " " |wc -m >>netsize_all.txt
+head -n 1 *.txt > tempB*.txt
+cat tempB*.txt | tr -d " " |wc -m > tempB*.txt
 #remuevo los archivos temporales
-rm $*.txt
+cat temp*.txt
 #imprimo los resultados
-cat netsize_all.txt
+rm temp*.txt
 
