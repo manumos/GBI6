0f877feda2c5896eb87ce7ddb477d8335039dcc0
manumos <manuela.moscoso@est.ikiam.edu.ec>
2021-12-11 19:02:48 -0500

    ahora se imprime cada archivo con el conteo de filas y columnas por separado pero se estan contando otros archivos que no son

--- netsize_all.sh
+++ netsize_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #imprimo el primer titulo y creo un archivo nuevo
-echo "POLINIZADORES" > netsize_all.txt
+#echo "POLINIZADORES" > netsize_all.txt
 #abro el directorio de interes
 #de todos los archivos .txt cuento las lineas y las guardo en el anterior archivo creado
-cd $1 |wc -l *.txt >> netsize_all.txt
+cd $1
+wc -l *.txt >> temp*.txt
+cat temp*.txt >> netsize_all.txt
+rm temp*.txt
 #imprimo el segundo titulo en el archivo creado
-echo "PLANTAS" >> netsize_all.txt
+#echo "PLANTAS" >> netsize_all.txt
 #selecciono la primera fila de los archivos .txt y la almaceno en un documento temporal
 head -n 1 *.txt > $1.txt
 #imprimo el documento temporal y  borro los espacios en blanco
