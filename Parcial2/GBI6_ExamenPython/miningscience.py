e34a31078e6f3ffe5d530c459fe2a84643e38521
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-02-22 13:00:01 -0500

    Cambios en el script

--- Parcial2/GBI6_ExamenPython/miningscience.py
+++ Parcial2/GBI6_ExamenPython/miningscience.py
@@ -1,14 +1,21 @@
 def download_pubmed(keyword): 
-    """Docstring download_pubmed"""
-    
-    
-    return 
+    """Descargar la data de pubmed"""
+    import Bio
+    from Bio.Seq import Seq
+    from Bio import Entrez
+    Entrez.email = "manuela.moscoso@est.ikiam.edu.ec.com"
+    resultados = Entrez.esearch(db="pubmed", 
+                        term="keyword[Title/Abstract]")
+    dic_resultados = Entrez.read(resultados)
+    resultados.close()
+    return dic_resultados.keys()
 
-def mining_pubs(tipo):
-    """Docstring mining_pubs"""
-    if tipo == "AD":
+#def mining_pubs(tipo):
+    #"""Según el tipo de dato extraer infromación específica"""
+    #import re
+    #if tipo == "AD":
         
     
-    return 
+    #return 
 
     
