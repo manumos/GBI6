c116af961fe27033a0cfeab7191e2acdbaf59e84
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-02-23 23:35:04 -0500

    He creado un borrador para probar las funciones antes de pasarlas al notebook

--- Parcial2/GBI6_ExamenPython/miningscience.py
+++ Parcial2/GBI6_ExamenPython/miningscience.py
@@ -1,21 +1,42 @@
-def download_pubmed(keyword): 
+#Importar los paquetes de interés
+import Bio
+from Bio import Entrez as en
+from bio import Medline as md
+import pandas as pd
+import numpy as np
+import re
+
+#Siempre decirle quien eres a NCBI
+Entrez.email = "manuela.moscoso@est.ikiam.edu.ec.com"
+
+#PRIMERA FUNCION
+def download_pubmed(x):
     """Descargar la data de pubmed"""
-    import Bio
-    from Bio.Seq import Seq
-    from Bio import Entrez
-    Entrez.email = "manuela.moscoso@est.ikiam.edu.ec.com"
-    resultados = Entrez.esearch(db="pubmed", 
-                        term="keyword[Title/Abstract]")
-    dic_resultados = Entrez.read(resultados)
-    resultados.close()
-    return dic_resultados.keys()
+    #definir termino de busqueda
+    busqueda=x+"[Title/Abstract]"
+    #realizar la busqueda
+    handle= en.esearch(db="pubmed", term=busqueda)
+    record = en.read(handle)
+    record ["IdList"]
+    #Descargar resultados de PUBMED
+    idlist = record["IdList"]
+    handle = en.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
+        return
+            records = Medline.parse(handle)
 
-#def mining_pubs(tipo):
-    #"""Según el tipo de dato extraer infromación específica"""
-    #import re
-    #if tipo == "AD":
-        
-    
-    #return 
+#SEGUNDA FUNCION
+def mining_pubs(y)
+    """Organizar la informacion en dataframes"""
+    if y==DP
+        #PMID y el DP_year
+        records["PMID"]["DP"]
+        return pd.dataframe()
 
-    
+    if y==AU
+        #PMID y el num_auth
+        records["PMID"]["AU"]
+        return pd.dataframe()
+    if y==AD
+        #Country y el num_auth.
+        records["PMID"]["AD"]["AU"]
+            return pd.dataframe()
