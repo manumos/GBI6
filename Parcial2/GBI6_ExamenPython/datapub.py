32879094ef3564467ea440a012064627a3d1c0a8
manumos <manuela.moscoso@est.ikiam.edu.ec>
2022-02-26 20:04:16 -0500

    Creacion del directorio y archivos

--- Parcial2/GBI6_ExamenPython/datapub.py
+++ Parcial2/GBI6_ExamenPython/datapub.py
@@ -15,20 +15,29 @@ def download_pubmed(keyword):
     """
     
     #importar paquetes
-    import Bio
     from Bio import Entrez as en
-    from Bio import Medline as md
+
     
     #notificar correo
     en.email = "manuela.moscoso@est.ikiam.edu.ec.com"
-
-    #definir termino de busqueda
-    busqueda=keyword+"[Title/Abstract]"
     
     #realizar la busqueda
-    handle= en.esearch(db="pubmed", term=busqueda)
+    handle= en.esearch(db="pubmed", term=keyword, usehistory="y",retmax=1000,retmode='xml')
     record = en.read(handle)
-    return (record.get("IdList"))
+    
+    #obtener ids
+    idlist = record["IdList"]
+    i_d=",".join(idlist)
+    
+    #buscar con los ids obtenidos
+    en.email = "manuela.moscoso@est.ikiam.edu.ec.com"
+    handle = en.efetch(db="pubmed", id=i_d, retmode="xml")
+    
+    
+    #la funcion arroja la lista de ids
+    respuesta = i_d.split(",")
+    return (respuesta)
+
 
 #SEGUNDA FUNCION
 def mining_pubs(keyword,tipo):
@@ -46,56 +55,75 @@ def mining_pubs(keyword,tipo):
                 """
     
     #importar paquetes
-    import Bio
     from Bio import Entrez as en
-    from Bio import Medline as md
-    import pandas as pan
-    import numpy as np
+    import pandas as pd
+    from collections import Counter
+    import re
  
     #notificar correo
     en.email = "manuela.moscoso@est.ikiam.edu.ec.com"
-    
-    #definir termino de busqueda
-    busqueda=keyword+"[Title/Abstract]"
+
 
     #realizar la busqueda
-    handle= en.esearch(db="pubmed", term=busqueda)
+    handle= en.esearch(db="pubmed", term=keyword,usehistory="y",retmax=100,retmode=xml)
     record = en.read(handle)
     idlist = record["IdList"]
-    handle = en.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
-
-    #almacenar los resultados para su manipulacion
-    records=md.parse(handle)
+    i_d=",".join(idlist)
+    #handle = en.efetch(db="pubmed", id=i_d, retmode="text")
+    #record = en.read(handle)
+   #POSIBLE MODIFICACION 
+    resultados=[]
+    for item in idlist:
+        with en.efetch(db="pubmed", rettype="gb", retmode="text", id=item)as handle:
+            resultados.append(en.read(handle))
+            return resultados
+    #almacenar datos     
     
-    #crear las listas con los resultados
-    pmid_dict=[]
-    au_dict=[]
-    ad_dict=[]
-    dp_dict=[]
+    #PMID
+    PMID = re.findall("PMID- (\d*)", resultados)
     
-    for record in records:   
-        #registrar los PMIDs    
-        pmid_dict.extend(record.get("PMID", "?"))
-        au_dict.extend(record.get("AU", "?"))
-        ad_dict.extend(record.get("AD", "?"))
-        dp_dict.extend(record.get("DP", "?"))
-        
-    #generar la sumatoria de autores
-    contador=[len (i) for i in au_dict]
+    #Fecha de publicacion (DP)
+    DP = re.findall("DP\s{2}-\s(\d{4})", resultados) 
     
-    #crear los data frames a partir de las listas obtenidas
+    #Autores (AU)
+    AU = resultados.split("PMID- ")
     
-    df_au=pan.DataFrame(list(zip(pmid_dict,contador)),columns=['PMID','num_auth'])
+    #a continuacion contar el numero de autores 
+    AU.pop(0)
+    n_au= []
+    for item in range(len(AU)):
+        autores = re.findall("AU -", AU[item])
+        au = (len(autores))
+        n_au.append(au)
     
-    df_ad=pan.DataFrame(list(zip(ad_dict,contador)),columns=['country','num_auth'])
+    #Afiliacion (AD)
+    #buscar unicamente los paises en cada direccion
+    resultados = re.sub(r"Av\.","", resultados)
+    AD = texto.split("AD  - ")
+    #contar las repeticiones por pais
+    n_ad = []
+    for item in range(len(AD)): 
+        pais = re.findall("\S, ([A-Za-z]*)\.", AD[item])
+        if not pais_n == []: 
+            if not len(pais_n) >= 2:  
+                if re.findall("^[A-Z]", pais_n[0]): 
+                    n_ad.append(pais_n[0])
+    repeticiones=Counter(n_paises)
+    resultado = {}
+    for elemento in repeticiones:
+        valor = repeticiones[elemento]
+        if valor != 1: 
+            resultado[elemento] = valor
+            rep_pais = pd.DataFrame()
+            rep_pais["country"] = resultado.keys()
+            rep_pais["num_auth"] = resultado.values()
+            
+    #Mostrar los rsultados de acuerdo al TIPO
+    if tipo == "DP":
+        return pd.DataFrame(list(zip(PMID,DP)),columns=['PMID','DP_year'])
     
-    df_dp=pan.DataFrame(list(zip(pmid_dict,dp_dict)),columns=['PMID','DP_year'])
-
-if tipo=='AU':
-    print (df_au)
-elif tipo=='AD':
-    print (df_ad)
-elif tipo=='DP':   
-    print (df_dp)
-else print('Tipo invalido')
+    elif tipo == "AU":
+        return pd.DataFrame(list(zip(PMID,n_au)),columns=['PMID','num_auth'])
     
+    elif tipo == "AD":
+        return rep.pais
